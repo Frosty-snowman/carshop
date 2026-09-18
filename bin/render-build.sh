3494d214d57ac9ee0ad1da8f65c65fe92f4e9d7b
nattapatpinr <nattapatpinr@odds.team>
2026-09-18 16:26:47 +0700

    fix blueprint

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,3 +4,6 @@ set -o errexit
 bundle install
 SECRET_KEY_BASE_DUMMY=1 bundle exec rails assets:precompile
 SECRET_KEY_BASE_DUMMY=1 bundle exec rails assets:clean
+
+# Free plan has no pre-deploy command; prepare DB during build.
+./bin/render-release.sh
